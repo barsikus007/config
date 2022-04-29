e4385aa6c3a233edb6a400b3a0ead34c20f939c2
barsikus007 <barsikus07@yandex.ru>
2022-04-29 11:38:34 +0300

    pwsh profile

--- windows/terminal/Microsoft.PowerShell_profile.ps1
+++ windows/terminal/Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,29 @@
-oh-my-posh init pwsh --config D:\projects\config\windows\terminal\own.omp.json | Invoke-Expression
-oh-my-posh completion powershell | Out-String | Invoke-Expression
-Import-Module posh-git
-Import-Module terminal-icons
+# starship
+Invoke-Expression (&starship init powershell)
+starship completions powershell | Out-String | Invoke-Expression
+
+# oh-my-posh
+# too slow ?
+# oh-my-posh init pwsh --config D:\projects\config\windows\terminal\own.omp.json | Invoke-Expression
+# oh-my-posh completion powershell | Out-String | Invoke-Expression
+
+# too slow
+# Import-Module posh-git
+# $GitPromptSettings.EnablePromptStatus=$false
+# $GitPromptSettings.EnableFileStatus=$false
+
+# too slow ?
+# Import-Module terminal-icons
 
 Invoke-Expression (&scoop-search --hook)
 Import-Module "$($(Get-Item $(Get-Command scoop.ps1).Path).Directory.Parent.FullName)\modules\scoop-completion"
+
+Set-PSReadLineOption -PredictionSource History
+# Set-PSReadlineOption -EditMode Vi
+# Set-PSReadlineOption -EditMode Emacs
+Set-PSReadlineKeyHandler -Key ctrl+d -Function DeleteCharOrExit
+Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
 
 Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
     param($wordToComplete, $commandAst, $cursorPosition)
