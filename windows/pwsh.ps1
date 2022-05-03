faadf09222150fbb4e3d1f4391cd354c964eacbf
Vladislav Reshetnikov <barsikus07@gmail.com>
2022-05-04 02:28:51 +0300

    terminal settings script install

--- windows/pwsh.ps1
+++ windows/pwsh.ps1
@@ -6,11 +6,19 @@ echo Executing:
 echo $PSScriptRoot\scoop\InstallScoop.ps1
 echo ''
 
-PowerShell.exe -ExecutionPolicy Bypass -File $PSScriptRoot\scoop\InstallScoop.ps1 $args  # D:\scoop
+# PowerShell.exe -ExecutionPolicy Bypass -File $PSScriptRoot\scoop\InstallScoop.ps1 $args  # D:\scoop
 
-mkdir ~\Documents\WindowsPowerShell
-mkdir ~\Documents\PowerShell
-cp windows\terminal\Microsoft.PowerShell_profile.ps1 $PROFILE
-mkdir ~\AppData\Local\clink
-cp windows\terminal\starship.lua ~\AppData\Local\clink\ -Recurse -Force
-cp .config\* ~\.config\
+# copy config
+mkdir ~\Documents\WindowsPowerShell -Force | Out-Null
+mkdir ~\Documents\PowerShell -Force | Out-Null
+cp $PSScriptRoot\terminal\Microsoft.PowerShell_profile.ps1 $PROFILE
+mkdir ~\AppData\Local\clink -Force | Out-Null
+cp $PSScriptRoot\terminal\starship.lua ~\AppData\Local\clink\ -Recurse -Force
+cp $PSScriptRoot\..\.config\* ~\.config\ -Recurse -Force
+cp $PSScriptRoot\terminal\icons ~\Pictures\icons -Recurse -Force
+
+echo 'Choose theme for terminal:'
+echo Light:
+echo "cp $PSScriptRoot\terminal\settings.json ~\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\ -Recurse -Force"
+echo Dark:
+echo "cp $PSScriptRoot\terminal\settings-dark.json ~\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json -Recurse -Force"
