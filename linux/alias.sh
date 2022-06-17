654af7c4ee5cc8e28a95abe52597043eac4eda3c
barsikus007 <barsikus07@yandex.ru>
2022-06-17 06:59:10 +0400

    clear docker logs alias

--- linux/alias.sh
+++ linux/alias.sh
@@ -5,6 +5,7 @@ alias h='history'
 alias hf='h|grp'
 alias u='sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt clean'
 alias dcu='docker compose up -d --build'
+alias cdl='sudo sh -c "truncate -s 0 /var/lib/docker/containers/*/*-json.log"'
 EOT
 
 . ~/.bashrc
