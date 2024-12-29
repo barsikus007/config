17a6a0a50f5fbf8ab5a2aa8f99826b631f064338
barsikus007 <barsikus07@gmail.com>
2024-12-30 02:57:24 +0300

    add logging

--- browser/userscripts/YouTubeFix.user.js
+++ browser/userscripts/YouTubeFix.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube Fix
 // @namespace    https://github.com/barsikus007/
-// @version      2.0
+// @version      2.0.1
 // @author       XpucT & AngusWR
 // @description  Force focus and scroll to player when mouse enters it & 'Video paused. Continue watching?' auto confirmer
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
@@ -16,6 +16,7 @@ const player = document.querySelector("video.video-stream");
 player.onmouseenter = () => {
   player.focus();
   scrollTo({ top: 0, behavior: "smooth" });
+  console.log("YouTube Fix:", "Player focused and scrolled to");
 };
 
 // Credits: https://greasyfork.org/en/scripts/377506-youtube-video-paused-continue-watching-auto-confirmer
@@ -30,6 +31,7 @@ setInterval(() => {
     for (let i = 0; i < popup.length; i++) {
       if (texts.includes(popup[i].innerText)) {
         popup[i].parentNode.parentNode.parentNode.querySelector("#confirm-button").click();
+        console.log("YouTube Fix:", "Continue watching confirmed in", popup[i].innerText);
       }
     }
   }
