74ef09dd3df5a8a28776d0fc8f3a8f3b1dd958d5
barsikus007 <barsikus07@gmail.com>
2025-05-04 14:13:37 +0300

    steam && proxy

--- nix/modules/gui/steam.nix
+++ nix/modules/gui/steam.nix
@@ -5,7 +5,44 @@
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     extraCompatPackages = [ pkgs.proton-ge-bin ];
-    # TODO
-    # package = pkgs.steam.override { withJava = true; };
+    package = pkgs.steam.override {
+      # TODO
+      # withJava = true;
+      extraPkgs = (
+        pkgs: with pkgs; [
+          gamemode
+        ]
+      );
+    };
+    # additional steam settings...
+    # e.g. remotePlay.openFirewall = true;
   };
+
+  # https://medium.com/@notquitethereyet_/gaming-on-nixos-%EF%B8%8F-f98506351a24
+  programs.gamescope.enable = true;
+  programs.gamemode.enable = true;
+  # TODO: https://wiki.nixos.org/wiki/Heroic_Games_Launcher#Optional_Dependencies
+  environment.systemPackages = with pkgs; [
+    protonup-qt
+
+    unstable.r2modman
+
+    mangohud
+
+    (lutris.override {
+      extraPkgs = pkgs: [
+        gamescope
+        gamemode
+      ];
+    })
+    (heroic.override {
+      extraPkgs = pkgs: [
+        gamescope
+        gamemode
+      ];
+    })
+    bottles
+
+    parsec-bin
+  ];
 }
