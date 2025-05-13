d9ac473b7c6f5b0c3a78b7acc813556f08dbf46d
barsikus007 <barsikus07@gmail.com>
2025-05-13 23:10:43 +0300

    sanitize deps

--- nix/packages/libfprint-27c6-521d.nix
+++ nix/packages/libfprint-27c6-521d.nix
@@ -4,25 +4,28 @@
   fetchFromGitHub,
   meson,
   ninja,
-  glib,
   pkg-config,
   cmake,
+  gtk-doc,
+  doctest,
+
+  glib,
   gusb,
-  cairo,
+  gobject-introspection,
+
   pixman,
   nss,
   openssl,
   libgudev,
-  gobject-introspection,
-  gtk-doc,
-  opencv,
-  doctest,
-  libfprint-tod,
   libfprint,
+
+  withTests ? false,
+  cairo,
 }:
 #! 27c6:5110 -> 27c6:521d
 #? https://github.com/lzc256/nur/blob/cfd1202a2b6988f408ef1116464c894f6d8d69be/pkgs/libfprint-27c6-5110/default.nix
 #? https://github.com/0x00002a/libfprint/compare/0x2a/dev/goodixtls-sigfm...Infinytum:libfprint:driver/goodix-521d
+#? https://aur.archlinux.org/packages/libfprint-goodix-521d
 stdenv.mkDerivation {
   pname = "libfprint-27c6-521d";
   version = "uwu-0";
@@ -52,19 +55,21 @@ stdenv.mkDerivation {
     gtk-doc
     doctest
   ];
-  buildInputs = [
-    glib
-    gusb
-    cairo
-    pixman
-    nss
-    gobject-introspection
-    openssl
-    libgudev
-    opencv
-    libfprint-tod
-    libfprint
-  ];
+  buildInputs =
+    [
+      glib
+      gusb
+      gobject-introspection
+
+      pixman
+      nss
+      openssl
+      libgudev
+      libfprint
+    ]
+    ++ lib.optionals withTests [
+      cairo
+    ];
   mesonBuildType = "release";
 
   passthru.driverPath = "/lib";
