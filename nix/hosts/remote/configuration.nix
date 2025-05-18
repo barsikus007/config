296170bc552286b4dd564adcc587b7e369de6435
barsikus007 <barsikus07@gmail.com>
2025-05-18 21:41:19 +0300

    Did you read the comment?

--- nix/hosts/remote/configuration.nix
+++ nix/hosts/remote/configuration.nix
@@ -6,7 +6,7 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
+  system.stateVersion = "25.05"; # Did you read the comment?
   networking.hostName = "remote"; # Define your hostname
 
   environment.systemPackages = (import ../../shared/lists/base.nix { inherit pkgs; });
