cb9c16ad27850739fca37a8f7867a2ed652dc245
barsikus007 <barsikus07@gmail.com>
2025-09-05 13:07:59 +0300

    add jdks to prism

--- nix/home/gui/minecraft.nix
+++ nix/home/gui/minecraft.nix
@@ -1,4 +1,13 @@
 { pkgs, ... }:
 {
-  home.packages = with pkgs; [ prismlauncher ];
+  home.packages = with pkgs; [
+    (prismlauncher.override {
+      jdks = [
+        # jdk8
+        graalvmPackages.graalvm-oracle_17
+        # jdk21
+        graalvmPackages.graalvm-oracle
+      ];
+    })
+  ];
 }
