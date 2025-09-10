5a97d1e1e18b183665de2d00059f8b5112bf536e
barsikus007 <barsikus07@gmail.com>
2025-09-10 14:54:49 +0300

    i dont fucking know......

--- nix/modules/gui/nekoray.nix
+++ nix/modules/gui/nekoray.nix
@@ -1,9 +1,29 @@
-{ pkgs, inputs, ... }:
 {
-  # imports = [ "${inputs.nixpkgs-unstable}/nixos/modules/programs/nekoray.nix" ];
-  programs.nekoray = {
+  pkgs,
+  ...
+}:
+let
+  repo =
+    # TODO rename after PR accepted
+    (builtins.getFlake "github:TomaSajt/nixpkgs/7f01041c58ffddc0acab9723dc9c2dced6fbd8cd").outPath;
+in
+{
+  imports = [
+    "${repo}/nixos/modules/programs/throne.nix"
+  ];
+  disabledModules = [
+    "programs/nekoray.nix"
+  ];
+  programs.throne = {
+    #? https://github.com/throneproj/Throne/issues/720#issuecomment-3223685757
+    #! Settings -> Basic settings -> Core -> Core Options -> Underlying DNS: `tcp://1.0.0.1`
     enable = true;
-    # package = pkgs.nekoray;
+    # package = "${repo}/pkgs/by-name/th/throne/package.nix";
+    package = (
+      pkgs.callPackage "${repo}/pkgs/by-name/th/throne/package.nix" {
+        protorpc = pkgs.callPackage "${repo}/pkgs/by-name/pr/protorpc/package.nix" { };
+      }
+    );
     tunMode = {
       enable = true;
       # setuid = true;
