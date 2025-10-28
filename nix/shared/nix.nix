21e854bc69a5a3fbd8e2dec6d7da890b747a6025
barsikus007 <barsikus07@gmail.com>
2025-10-28 07:27:25 +0300

    revert cloudflare mirror due to russia bans

--- nix/shared/nix.nix
+++ nix/shared/nix.nix
@@ -1,14 +1,22 @@
-{ lib, pkgs, ... }:
+{
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
 {
   nix.package = pkgs.nix;
+  nix.registry = {
+    nixpkgs.flake = inputs.nixpkgs;
+  };
   nix.settings = {
     experimental-features = [
       "nix-command"
       "flakes"
     ];
     substituters = lib.mkForce [
-      #? "https://cache.nixos.org/" cloudflare mirror, uses original keys
-      "https://nixos-cache-proxy.cofob.dev"
+      "https://cache.nixos.org/"
+      # "https://nixos-cache-proxy.cofob.dev" # ? cloudflare mirror, uses original keys
 
       "https://nix-community.cachix.org"
     ];
