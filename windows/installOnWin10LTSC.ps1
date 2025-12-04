717c00bd7b064277bea3d4f56a96b52e83492486
barsikus007 <barsikus07@gmail.com>
2025-12-05 00:33:57 +0300

    shitty posh hotfix

--- windows/installOnWin10LTSC.ps1
+++ windows/installOnWin10LTSC.ps1
@@ -12,10 +12,6 @@ Invoke-RestMethod https://raw.githubusercontent.com/barsikus007/config/refs/head
 Invoke-RestMethod https://raw.githubusercontent.com/barsikus007/config/refs/heads/master/windows/scoop/20SoftHighPriority.ps1 | Invoke-Expression
 
 
-pwsh.exe
-cd && git clone https://github.com/barsikus007/config --depth 1 && cd ~\config\ && sudo .\windows\pwsh.ps1 && cd -
-
-
 Write-Host "Notes from scoop packages" -ForegroundColor Green
 # TODO parse them programmatically
 $SCOOP_HOME = $(If (Test-Path env:SCOOP) { $env:SCOOP } Else { ($env:GIT_INSTALL_ROOT -split "scoop")[0]+"scoop" })
@@ -25,3 +21,6 @@ reg import "$SCOOP_HOME\apps\notepadplusplus\current\install-context.reg"
 
 
 winget install -e --id Microsoft.Edge -h --force
+
+
+pwsh.exe -Command 'cd && git clone https://github.com/barsikus007/config --depth 1 && cd ~\config\ && sudo .\windows\pwsh.ps1 && cd -'
