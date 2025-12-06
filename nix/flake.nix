40894de4c29dee1f418ff9a8673a9e99e0f19955
barsikus007 <barsikus07@gmail.com>
2025-12-06 08:32:03 +0300

    nix-on-droid fix??

--- nix/flake.nix
+++ nix/flake.nix
@@ -266,25 +266,6 @@
         #   username = "nix-on-droid";
         #   flakePath = "/data/data/com.termux.nix/files/home";
         # };
-        nix = {
-          extraOptions = ''
-            experimental-features = nix-command flakes pipe-operators
-          '';
-          registry = {
-            nixpkgs.flake = inputs.nixpkgs;
-          };
-          substituters = [
-            "https://cache.nixos.org"
-            # "https://nixos-cache-proxy.cofob.dev" # ? cloudflare mirror, uses original keys
-
-            "https://nix-community.cachix.org"
-          ];
-          trusted-public-keys = [
-            "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-
-            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-          ];
-        };
         modules = [
           #! ./shared/nix.nix
           ./nix-on-droid.nix
--- nix/nix-on-droid.nix
+++ nix/nix-on-droid.nix
@@ -1,5 +1,27 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
 {
+  # modules = [
+  #   #! ./shared/nix.nix
+  # ];
+  nix = {
+    extraOptions = ''
+      experimental-features = nix-command flakes pipe-operators
+    '';
+    registry = {
+      nixpkgs.flake = inputs.nixpkgs;
+    };
+    substituters = [
+      "https://cache.nixos.org"
+      # "https://nixos-cache-proxy.cofob.dev" # ? cloudflare mirror, uses original keys
+
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
   #? https://nix-community.github.io/nix-on-droid/nix-on-droid-options.html#sec-options
   android-integration.am.enable = true;
   android-integration.termux-setup-storage.enable = true;
