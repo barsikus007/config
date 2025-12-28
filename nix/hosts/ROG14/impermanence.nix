a41836e18f446112ef123ef0b0d725fd75d7011c
barsikus007 <barsikus07@gmail.com>
2025-12-28 15:46:05 +0300

    impermanence reached, horray!

--- nix/hosts/ROG14/impermanence.nix
+++ nix/hosts/ROG14/impermanence.nix
@@ -1,17 +1,21 @@
 {
+  lib,
   config,
   inputs,
   username,
   ...
 }:
 #? https://nix-community.github.io/preservation/impermanence-migration.html maybe
+let
+  inherit (config.fileSystems."/nix") device;
+in
 {
   imports = [
     inputs.impermanence.nixosModules.impermanence
   ];
   #? sudo btrfs subvolume create /@persistent
   fileSystems."/persistent" = {
-    inherit (config.fileSystems."/nix") device;
+    inherit device;
     fsType = "btrfs";
     neededForBoot = true;
     options = [
@@ -20,11 +24,37 @@
       "compress=zstd"
     ];
   };
+  boot.initrd.postResumeCommands = lib.mkAfter ''
+    mkdir /btrfs_tmp
+    mount ${device} /btrfs_tmp
+    if [[ -e /btrfs_tmp/@ ]]; then
+      mkdir -p /btrfs_tmp/@-old_roots
+      timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/@)" "+%Y-%m-%-d_%H:%M:%S")
+      mv /btrfs_tmp/@ "/btrfs_tmp/@-old_roots/$timestamp"
+    fi
+
+    delete_subvolume_recursively() {
+      IFS=$'\n'
+      for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+        delete_subvolume_recursively "/btrfs_tmp/$i"
+      done
+      btrfs subvolume delete "$1"
+    }
+
+    #? delete old roots older than 7 days
+    for i in $(find /btrfs_tmp/@-old_roots/ -maxdepth 1 -mtime +7); do
+      delete_subvolume_recursively "$i"
+    done
+
+    btrfs subvolume create /btrfs_tmp/@
+    umount /btrfs_tmp
+  '';
+
   # sudo mkdir -p /persistent/etc/NetworkManager /persistent/var/{db,log,lib}
   # sudo cp -ax --reflink=always ...
   environment.persistence."/persistent" = {
     # enable = false; # ? NB: Defaults to true, not needed
-    # hideMounts = true; # ? toogle it after test
+    hideMounts = true;
 
     # sudo cp -ax --reflink=always /var/lib{bluetooth,btrfs,cups,fwupd,fprint,lastlog,misc,NetworkManager,nixos,power-profiles-daemon,private,sbctl,sddm,swtpm-localca,upower,waydroid} /persistent/var/lib
     # sudo mkdir /var/lib/{AccountsService,systemd}
@@ -34,6 +64,7 @@
       # "/etc/asusd" # TODO: asus
       "/etc/NetworkManager/system-connections"
       "/etc/nixos" # TODO: secrets
+      "/etc/shadow" # TODO: secrets
       "/var/db" # ? ./sudo/lectured/$(id -u)
       "/var/log"
       # "/var/lib/AccountsService/users" # TODO: is this needed? plasma
@@ -50,7 +81,7 @@
       # "/var/lib/power-profiles-daemon" # TODO: is this needed? my experiments with power toggling
       # "/var/lib/private" # TODO: is this needed? rustdesk
       # "/var/lib/sbctl" # TODO: is this needed? secure boot
-      # "/var/lib/sddm" # TODO: is this needed? sddn
+      # "/var/lib/sddm" # TODO: is this needed?
       # "/var/lib/swtpm-localca" # TODO: is this needed? tpm for libvirtd
       # "/var/lib/systemd/backlight" # TODO: is this needed? keyboard backlight
       # "/var/lib/systemd/catalog" # TODO: is this needed? creates auto, binary database for the Journal Message Catalog (extended log descriptions)
@@ -81,7 +112,7 @@
 
         "config"
         "hax"
-        "projects" # ! symlink
+        # "projects" # ! symlink
         "Sync"
 
         # TODO: https://wiki.nixos.org/wiki/Impermanence#Example_3
@@ -115,26 +146,23 @@
         ".steam"
         ".wine"
 
-        {
-          directory = ".ssh"; # ! symlink
-          mode = "0700";
-        }
+        # {
+        #   directory = ".ssh"; # ! symlink
+        #   mode = "0700";
+        # }
       ];
       files = [
         # ".zcompdump" # TODO: is this needed?
         ".zsh_history"
-        "awg0.conf" # ! symlink
+        # "awg0.conf" # ! symlink
       ];
     };
   };
-  # systemd.tmpfiles.rules = [
-  #   # Syntax: Type Path Mode User Group Age Argument
-  #   # L+ = Create symlink, remove existing file if necessary
-
-  #   "L+ /home/${username}/projects - - - - /run/media/${username}/Data/projects/"
-
-  #   "L+ /home/${username}/.ssh - - - - /home/${username}/Sync/home/.ssh/"
-
-  #   "L+ /home/${username}/awg0.conf - - - - /home/${username}/Sync/wg/donstux-linux-49.conf"
-  # ];
+  systemd.tmpfiles.rules = [
+    # Syntax: Type Path Mode User Group Age Argument
+    # L+ = Create symlink, remove existing file if necessary
+    "L+ /home/${username}/awg0.conf - ${username} - - /home/${username}/Sync/wg/donstux-linux-49.conf"
+    "L+ /home/${username}/.ssh - ${username} - - /home/${username}/Sync/home/.ssh/"
+    "L+ /home/${username}/projects - ${username} - - /run/media/${username}/Data/projects/"
+  ];
 }
