d1b2550dd4a3fed09546e99acd33acdc8721e43e
barsikus007 <barsikus07@gmail.com>
2026-03-16 23:24:09 +0300

    fix nix mirrors again

--- nix/shared/nix.nix
+++ nix/shared/nix.nix
@@ -25,7 +25,7 @@ let
       connect-timeout = 4;
 
       substituters = lib.mkForce [
-        # "https://cache.nixos.org"
+        "https://cache.nixos.org"
         # "https://cache.nixos.kz" # ? returned a lot "Timeout was reached" errors
         "https://mirror.yandex.ru/nixos"
         # "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store" # ? https://github.com/dramforever/mirror-web/blob/d7e263d4fe9a9e3078f819468cec18e1c11cf832/_posts/help/2019-02-17-nix.md
@@ -37,7 +37,9 @@ let
 
         "https://attic.xuyh0120.win/lantian"
       ];
-      trusted-public-keys = lib.mkAfter [
+      trusted-public-keys = lib.mkForce [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
 
         "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
