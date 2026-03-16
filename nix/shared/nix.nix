50816b3510fb2ea0c30e3c1286ac88536ba4bb57
barsikus007 <barsikus07@gmail.com>
2026-03-16 03:39:23 +0300

    refactor nix settings

--- nix/shared/nix.nix
+++ nix/shared/nix.nix
@@ -5,46 +5,55 @@
   inputs,
   ...
 }:
-{
-  nix =
-    lib.attrsets.optionalAttrs (_class == "nixos") {
+let
+  nix = {
+    package = pkgs.nix;
+    registry = {
+      nixpkgs.flake = inputs.nixpkgs;
+      # master.flake = inputs.nixpkgs-master;
+    };
+    settings = {
+      auto-optimise-store = true;
+      use-xdg-base-directories = true;
+      experimental-features = [
+        "nix-command"
+        "flakes"
+        "pipe-operators"
+      ];
+
+      stalled-download-timeout = 4;
+      connect-timeout = 4;
+
+      substituters = lib.mkForce [
+        # "https://cache.nixos.org"
+        # "https://cache.nixos.kz" # ? returned a lot "Timeout was reached" errors
+        "https://mirror.yandex.ru/nixos"
+        # "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store" # ? https://github.com/dramforever/mirror-web/blob/d7e263d4fe9a9e3078f819468cec18e1c11cf832/_posts/help/2019-02-17-nix.md
+        "https://ncproxy.vizqq.cc"
+        "https://nixos-cache-proxy.cofob.dev" # ? cloudflare mirror, uses original keys
+        # "https://nixos-cache-proxy.sweetdogs.ru" # ? seems died
+
+        "https://nix-community.cachix.org"
+
+        "https://attic.xuyh0120.win/lantian"
+      ];
+      trusted-public-keys = lib.mkAfter [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+
+        "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
+      ];
+    };
+  };
+in
+# TODO: _class == "nixOnDroid"
+if (_class == "nixos") then
+  {
+    nix = {
       channel.enable = false;
     }
-    // {
-      package = pkgs.nix;
-      registry = {
-        nixpkgs.flake = inputs.nixpkgs;
-      };
-      settings = {
-        auto-optimise-store = true;
-        use-xdg-base-directories = true;
-        experimental-features = [
-          "nix-command"
-          "flakes"
-          "pipe-operators"
-        ];
-
-        stalled-download-timeout = 4;
-        connect-timeout = 4;
-
-        substituters = lib.mkForce [
-          # "https://cache.nixos.org"
-          # "https://cache.nixos.kz" # ? returned a lot "Timeout was reached" errors
-          "https://mirror.yandex.ru/nixos"
-          # "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store" # ? https://github.com/dramforever/mirror-web/blob/d7e263d4fe9a9e3078f819468cec18e1c11cf832/_posts/help/2019-02-17-nix.md
-          "https://ncproxy.vizqq.cc"
-          "https://nixos-cache-proxy.cofob.dev" # ? cloudflare mirror, uses original keys
-          # "https://nixos-cache-proxy.sweetdogs.ru" # ? seems died
-
-          "https://nix-community.cachix.org"
-
-          "https://attic.xuyh0120.win/lantian"
-        ];
-        trusted-public-keys = lib.mkAfter [
-          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-
-          "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
-        ];
-      };
-    };
-}
+    // nix;
+  }
+else
+  {
+    inherit nix;
+  }
