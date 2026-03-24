8769cc1732ca5acf0934f03a10e6fb35311bdbf5
barsikus007 <barsikus07@gmail.com>
2026-03-24 06:59:57 +0300

    sanitize nix settings

--- nix/modules/cachyos-kernel.nix
+++ nix/modules/cachyos-kernel.nix
@@ -1,9 +1,18 @@
-{ pkgs, inputs, ... }:
+{
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
 {
   #! add this to flake inputs
   #? nix-cachyos-kernel.url = "github:xddxdd/nix-cachyos-kernel/release";
   nixpkgs.overlays = [ inputs.nix-cachyos-kernel.overlays.pinned ];
-  # boot.kernelPackages = pkgs.lib.mkForce pkgs.cachyosKernels.linuxPackages-cachyos-latest;
-  # boot.kernelPackages = pkgs.lib.mkForce pkgs.cachyosKernels.linuxPackages-cachyos-bore;
-  boot.kernelPackages = pkgs.lib.mkForce pkgs.cachyosKernels.linuxPackages-cachyos-bore-lto;
+  # boot.kernelPackages = lib.mkForce pkgs.cachyosKernels.linuxPackages-cachyos-latest;
+  # boot.kernelPackages = lib.mkForce pkgs.cachyosKernels.linuxPackages-cachyos-bore;
+  boot.kernelPackages = lib.mkForce pkgs.cachyosKernels.linuxPackages-cachyos-bore-lto;
+  nix.settings.substituters = lib.mkAfter [ "https://attic.xuyh0120.win/lantian" ];
+  nix.settings.trusted-public-keys = lib.mkAfter [
+    "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
+  ];
 }
--- nix/shared/nix.nix
+++ nix/shared/nix.nix
@@ -7,7 +7,7 @@
 }:
 let
   nix = {
-    package = pkgs.nix;
+    package = lib.mkDefault pkgs.nix;
     registry = {
       nixpkgs.flake = inputs.nixpkgs;
       # master.flake = inputs.nixpkgs-master;
@@ -24,7 +24,7 @@ let
       stalled-download-timeout = 4;
       connect-timeout = 4;
 
-      substituters = lib.mkForce [
+      substituters = lib.mkBefore [
         "https://cache.nixos.org"
         # "https://cache.nixos.kz" # ? returned a lot "Timeout was reached" errors
         "https://mirror.yandex.ru/nixos"
@@ -34,15 +34,11 @@ let
         # "https://nixos-cache-proxy.sweetdogs.ru" # ? seems died
 
         "https://nix-community.cachix.org"
-
-        "https://attic.xuyh0120.win/lantian"
       ];
-      trusted-public-keys = lib.mkForce [
+      trusted-public-keys = lib.mkBefore [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
 
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-
-        "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
       ];
     };
   };
