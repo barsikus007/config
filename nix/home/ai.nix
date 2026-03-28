94476dcd23698d7f225fb657a7a047314ed904de
barsikus007 <barsikus07@gmail.com>
2026-03-28 11:14:09 +0300

    allow "safe" commands for AI dum

--- nix/home/ai.nix
+++ nix/home/ai.nix
@@ -1,4 +1,23 @@
 {
   programs.codex.enable = true;
-  programs.gemini-cli.enable = true;
+  programs.gemini-cli = {
+    enable = true;
+    policies."allowed_run_shell_commands".rule =
+      map
+        (command: {
+          toolName = "run_shell_command";
+          commandPrefix = command;
+          decision = "allow";
+          priority = 100;
+        })
+        [
+          "ls"
+          "cat"
+          "curl"
+          "grep"
+          "head"
+          "git status"
+          "git log"
+        ];
+  };
 }
