fd8d77c5b2865284d57b82c1ce77c9203a7e8257
barsikus007 <barsikus07@gmail.com>
2026-04-19 23:15:50 +0300

    sane theming 2/2

--- nix/home/desktop/style/uniform-look.nix
+++ nix/home/desktop/style/uniform-look.nix
@@ -4,16 +4,27 @@
   config,
   ...
 }:
+#? https://wiki.archlinux.org/title/Uniform_look_for_Qt_and_GTK_applications
 {
-  home.sessionVariables.GTK2_RC_FILES = config.gtk.gtk2.configLocation;
-  gtk = {
-    gtk2 = {
-      configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
-      iconTheme.package = with pkgs; gnome-icon-theme;
-      iconTheme.name = "gnome";
+  #? stylix uses kvantum for theming, hardcoding svg (which is used for element shapes)
+  #? I don't like these shapes so I decided to just rollback to breeze
+  qt = {
+    enable = true;
+    platformTheme.name = lib.mkForce "kde";
+    style = {
+      name = lib.mkForce "breeze";
+      package = lib.mkForce pkgs.kdePackages.breeze;
     };
+  };
+
+  gtk = {
+    #? some icons are missing with breeze-icons in gtk2
+    gtk2.iconTheme.package = with pkgs; gnome-icon-theme;
+    gtk2.iconTheme.name = "gnome";
+    #! set by stylix to adw-gtk3 for no reason: https://github.com/nix-community/stylix/blob/e3861617645a43c9bbefde1aa6ac54dd0a44bfa9/modules/gtk/hm.nix#L59
     theme.package = lib.mkForce (with pkgs; kdePackages.breeze-gtk);
     theme.name = lib.mkForce (if (config.stylix.polarity == "light") then "Breeze" else "Breeze-Dark");
-    # TODO: gtk.gtk4.theme: https://nix-community.github.io/home-manager/release-notes.xhtml#sec-release-26.05-state-version-changes
+    #? gtk.gtk4.theme: https://nix-community.github.io/home-manager/release-notes.xhtml#sec-release-26.05-state-version-changes
+    #! https://github.com/nix-community/home-manager/blob/d401492e2acd4fea42f7705a3c266cea739c9c36/modules/misc/gtk/gtk4.nix#L69
   };
 }
