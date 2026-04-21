2972d068fb0c7e47a690a73838f682fb4a81d816
barsikus007 <barsikus07@gmail.com>
2026-04-21 11:29:18 +0300

    fix wg auto local

--- nix/modules/wireguard.nix
+++ nix/modules/wireguard.nix
@@ -24,19 +24,24 @@
   ];
 
   sops.secrets."hosts/NAS/router/ssid" = { };
-  networking.networkmanager.dispatcherScripts = [
-    {
-      source = pkgs.writeScript "local-wg-auto" ''
-        #!/bin/sh
-        if [ "$2" = "up" ]; then
+  networking.networkmanager.dispatcherScripts =
+    let
+      wifiIface = "wlp2s0";
+    in
+    [
+      #? https://networkmanager.dev/docs/api/latest/NetworkManager-dispatcher.html
+      {
+        source = pkgs.writeScript "local-wg-auto" ''
+          #!/bin/sh
+          [ "$DEVICE_IFACE" != "${wifiIface}" ] && exit 0
+          [ "$2" != "up" ] && exit 0
           if [ "$CONNECTION_ID" = $(cat ${config.sops.secrets."hosts/NAS/router/ssid".path}) ]; then
             ${pkgs.systemd}/bin/systemctl stop wg-quick-wg0local
           else
             ${pkgs.systemd}/bin/systemctl start wg-quick-wg0local
           fi
-        fi
-      '';
-      type = "basic";
-    }
-  ];
+        '';
+        type = "basic";
+      }
+    ];
 }
