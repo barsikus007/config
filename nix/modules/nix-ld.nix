8d4c2d4f0a5de2a4d99f67c42e6f38297c23a2fc
barsikus007 <barsikus07@gmail.com>
2026-06-08 01:29:37 +0300

    nix-ld: implement includeClosures for recursive steam-run libs inclusion

--- nix/modules/nix-ld.nix
+++ nix/modules/nix-ld.nix
@@ -4,38 +4,28 @@
 {
   programs.nix-ld.enable = true;
   programs.nix-ld.libraries =
-    with pkgs;
-    builtins.concatLists [
-      #? nix-locate lib/libgobject-2.0.so.0
-      [
-        nss
-        nspr
-        cups
-        # fuse3
-        # icu
-        expat
-        # vulkan-headers
-        # vulkan-tools
-
-        libx11
-        libxcb
-        libxcomposite
-        libxdamage
-        libxext
-        libxfixes
-        libxkbcommon
-        libxrandr
-
-        at-spi2-atk
-        alsa-lib
-        cairo
-        dbus
-        freetype
-        gdk-pixbuf
-        glib
-        gtk3
-        pango
-      ]
-      (steam-run.args.multiPkgs pkgs)
+    let
+      closureRoots =
+        with pkgs;
+        builtins.concatLists [
+          #? nix-locate lib/libgobject-2.0.so.0
+          [
+            nss
+            # fuse3
+            # vulkan-headers
+            # vulkan-tools
+          ]
+          (steam-run.args.targetPkgs pkgs)
+          (steam-run.args.multiPkgs pkgs)
+        ];
+    in
+    [
+      (pkgs.buildEnv {
+        name = "nix-ld-closure-libs";
+        paths = closureRoots;
+        pathsToLink = [ "/lib" ];
+        includeClosures = true;
+        ignoreCollisions = true;
+      })
     ];
 }
