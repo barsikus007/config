0997fc3ec8e455404d90e63e27ffa54d9b67091c
barsikus007 <barsikus07@gmail.com>
2026-07-13 11:53:23 +0300

    vicinae initial

--- nix/home/desktop/environment/launcher.nix
+++ nix/home/desktop/environment/launcher.nix
@@ -2,6 +2,7 @@
   lib,
   pkgs,
   config,
+  flakePath,
   ...
 }:
 {
@@ -50,5 +51,22 @@
     Install = {
       WantedBy = [ "graphical-session.target" ];
     };
+  };
+  programs.vicinae = {
+    enable = true;
+    systemd.enable = true;
+    # extensions = [];
+    settings = {
+      close_on_focus_loss = true;
+      #? controlled by niri
+      launcher_window.client_side_decorations.enabled = true;
+
+      launcher_window.layer_shell.layer = "overlay";
+    };
+  };
+  xdg.configFile."vicinae/settings.json".target = "vicinae/settings-static.json";
+  xdg.configFile."vicinae/settings.json-real" = {
+    target = "vicinae/settings.json";
+    source = config.lib.file.mkOutOfStoreSymlink "${flakePath}/.config/vicinae/settings.json";
   };
 }
